c934bb891070664167040f1bfe88f4eb76fda0ea
Theodor Nolte <theno@gmx.net>
2019-06-06 14:16:13 +0200

    Only check for write permission if archive-dir exists

--- run-backup.sh
+++ run-backup.sh
@@ -92,8 +92,7 @@ check_dirs () {
     if [ ! -d "$ARCHIVE_DIR" ]; then
         echo "* ARCHIVE_DIR='$ARCHIVE_DIR' is not a directory"
         error=true
-    fi
-    if [ ! -w "$ARCHIVE_DIR" ]; then
+    elif [ ! -w "$ARCHIVE_DIR" ]; then
         echo "* cannot write to ARCHIVE_DIR='$ARCHIVE_DIR' (check permissions)"
         error=true
     fi
